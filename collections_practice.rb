d6e709444e4cd3e2a5a887b81dbbc8163dc7d46a
danmj <daniel.jung@stern.nyu.edu>
2018-08-27 21:33:54 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+  if a == b
+    0
+  elsif a < b
+    -1
+  elsif a > b
+    1
+  end
+end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+  if a == b
+    0
+  elsif a < b
+    1
+  elsif a > b
+    -1
+  end
+end
+end
+
+def sort_array_char_count(array)
+  newarr = arr.sort_by(&:length)
+  newarr
+end
+
+def swap_elements(array)
+  
+end
